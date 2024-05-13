19ca60744e9f6bd632e21cb06ca58fc5d801e4e8
Juan Pablo Nunez <juanpablo.nunez@assuresoft.com>
2024-05-13 09:16:43 -0400

    Initial Commit

---
+++ Creatine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Creatine
+//
+//  Created by Juan Pablo Nunez on 13/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Creatine/CreatineApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreatineApp.swift
+//  Creatine
+//
+//  Created by Juan Pablo Nunez on 13/5/24.
+//
+
+import SwiftUI
+
+@main
+struct CreatineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
